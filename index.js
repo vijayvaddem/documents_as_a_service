90e502cc79a954c8867accbf41c2d9edc3a29bfc
vaddv <vijay.vaddem@gmail.com>
2020-08-29 18:15:19 +0530

    Adding documents as a service as a replacement for Posts

---
+++ index.js
@@ -0,0 +1,63 @@
+//initialize express
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+
+//create app
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+//listen on a port
+app.listen("4000", () => {
+  console.log("This is v51");
+  console.log("listening on 4000");
+});
+
+// store data
+const documents = {};
+
+//API to get data
+app.get("/documents", (req, res) => {
+  res.send(documents);
+});
+
+//API to post data
+app.post("/documents", async (req, res) => {
+  //generate random id
+  const id = randomBytes(4).toString("hex");
+
+  // extract title from req body
+  const title = req.body.title;
+  const content = req.body.content;
+
+  console.log("Title", title);
+  console.log("Content", content);
+
+  //add to documents array
+  documents[id] = {
+    id,
+    title,
+    content,
+  };
+
+  //post to event bus
+  await axios.post("http://localhost:4005/events", {
+    type: "documentCreated",
+    data: {
+      id,
+      title,
+      content,
+    },
+  });
+
+  res.status("201").send(documents[id]);
+});
+
+//post handler for incoming events request
+app.post("/event", (req, res) => {
+  console.log("Incoming event received for document srv:", req.body.type);
+  res.send({});
+});
